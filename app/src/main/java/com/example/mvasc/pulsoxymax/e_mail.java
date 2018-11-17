a04d13d63bf1627fdb178c7400e2f03577ea016d
Mateo <be17b021@technikum-wien.at>
2018-11-17 15:46:41 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mvasc/pulsoxymax/e_mail.java
@@ -0,0 +1,13 @@
+package com.example.mvasc.pulsoxymax;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class e_mail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_e_mail);
+    }
+}
