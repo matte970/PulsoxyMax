9503ff6a66b17ca47886840b8a9285d1a57d8c39
Mateo <be17b021@technikum-wien.at>
2018-12-02 01:23:57 +0100

    Mateo OnClick + Correct build

--- app/src/main/java/com/example/mvasc/pulsoxymax/home.java
+++ app/src/main/java/com/example/mvasc/pulsoxymax/home.java
@@ -1,5 +1,6 @@
 package com.example.mvasc.pulsoxymax;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,30 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void start_sleep_mode(android.view.View next){
+
+    }
+
+    public void go_to_stats(android.view.View next){
+        Intent i = new Intent(this, stats.class);
+        startActivity(i);
+    }
+
+    public void go_to_options(android.view.View next){
+        Intent i = new Intent(this, set_up.class);
+        startActivity(i);
+    }
+
+    public void go_to_start(android.view.View next){
+        Intent i = new Intent(this, start.class);
+        startActivity(i);
+    }
+
+
+    public void go_to_liveView(android.view.View next){
+        Intent i = new Intent(this, live_values.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/mvasc/pulsoxymax/set_up.java
+++ app/src/main/java/com/example/mvasc/pulsoxymax/set_up.java
@@ -1,5 +1,6 @@
 package com.example.mvasc.pulsoxymax;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,30 @@ public class set_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_up);
+    }
+
+    public void go_to_start(android.view.View next){
+        Intent i = new Intent(this, start.class);
+        startActivity(i);
+    }
+
+    public void go_to_home(android.view.View next){
+        Intent i = new Intent(this, home.class);
+        startActivity(i);
+    }
+
+    public void go_to_tones(android.view.View next){
+        Intent i = new Intent(this, tones.class);
+        startActivity(i);
+    }
+
+    public void go_to_limits(android.view.View next){
+        Intent i = new Intent(this, limit.class);
+        startActivity(i);
+    }
+
+    public void go_to_e_mail(android.view.View next){
+        Intent i = new Intent(this, e_mail.class);
+        startActivity(i);
     }
 }
