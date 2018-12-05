fcc3876e84fe7e0b016245b70927bd15d89cf0ea
Justyna87 <justyna.przezdziecka@gmail.com>
2018-12-05 12:09:42 +0100

    Start_click_funktioniert

--- app/src/main/java/com/example/mvasc/pulsoxymax/stats.java
+++ app/src/main/java/com/example/mvasc/pulsoxymax/stats.java
@@ -2,12 +2,28 @@ package com.example.mvasc.pulsoxymax;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.content.Intent;
 
 public class stats extends AppCompatActivity {
 
+    Button button;              //create new object
+    AutoCompleteTextView acText;
+    String[] hours = {"24 Hours", "48 Hours", "7 Days", "1 Month" };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stats);
+        setContentView(R.layout.activity_e_mail);
+        button = (Button) findViewById(R.id.button1);
+        acText = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.select_dialog_item, hours);
+        acText.setThreshold(1);
+        acText.setAdapter(adapter);
+    }
+    public void go_to_options(android.view.View next){
+        Intent i = new Intent(this, stats.class);
+        startActivity(i);
     }
 }
