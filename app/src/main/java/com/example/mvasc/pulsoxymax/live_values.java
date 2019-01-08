8b14fd876308a9f0e37d907bbdbfda4825f41d56
doris <doris.weninger1@gmail.com>
2019-01-08 20:52:22 +0100

    database created when live-values view entered database pre-filled with values entering live-values view is printing records from database to console (temporarly)

--- app/src/main/java/com/example/mvasc/pulsoxymax/live_values.java
+++ app/src/main/java/com/example/mvasc/pulsoxymax/live_values.java
@@ -1,7 +1,9 @@
 package com.example.mvasc.pulsoxymax;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class live_values extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class live_values extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_values);
+
+        System.out.println("livevalues");
+
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        Cursor cursor = db.query(DatabaseHelper.DB_NAME, new String[]{"oxy", "time"}, null, null, null, null, null);
+
+
+        if (cursor.moveToFirst()) {
+            Long oxy = cursor.getLong(0);
+            Long time = cursor.getLong(1);
+            System.out.println("juhu " + oxy);
+            System.out.println("bbb " + time);
+            while (cursor.moveToNext()) {
+                oxy = cursor.getLong(0);
+                time = cursor.getLong(1);
+                System.out.println("juhu " + oxy);
+                System.out.println("bbb " + time);
+            }
+
+        } else {
+            System.out.println("oh nein");
+        }
     }
 }
