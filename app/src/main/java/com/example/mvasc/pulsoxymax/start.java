1cda5ae0bcadb35c47b7ca29fe1888b45fd8a1f2
Mateo <be17b021@technikum-wien.at>
2019-01-15 13:26:59 +0100

    welcome back tones v3

--- app/src/main/java/com/example/mvasc/pulsoxymax/start.java
+++ app/src/main/java/com/example/mvasc/pulsoxymax/start.java
@@ -1,8 +1,11 @@
 package com.example.mvasc.pulsoxymax;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class start extends AppCompatActivity {
 
@@ -10,6 +13,12 @@ public class start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        TextView textView =(TextView) findViewById(R.id.textView2);
+
+        SharedPreferences sharedPref = getSharedPreferences("userinfo", Context.MODE_PRIVATE);
+        String username = sharedPref.getString("username", "");
+        textView.setText("Welcome back \n"+username);
+
     }
     public void go_to_home(android.view.View next){
         Intent i = new Intent(this, home.class);
